f4a04e5446266279a3107a1d0bda706678bd0c6e
tkynyk <gmelnuko@gmail.com>
2017-10-16 21:41:06 +0000

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+function fib(n) {
+    if (n === 0) { //比較===
+        return 0;
+    } else if (n ===1){
+        return 1;
+    }
+    return fib(n -1) + fib(n - 2);
+}
+const length = 40;
+for (let i = 0; i <= length; i++) { //forの中にそのまま40と入れない方が良いmagic number
+    console.log(fib(i));
+}
